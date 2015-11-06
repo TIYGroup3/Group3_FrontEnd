0ff0100cb7d25c7b688e2e2eec5cfe5b46e5f01d
Cary Duncan <duncan.cary@gmail.com>
2015-11-06 14:33:10 -0500

    Added deck_id to dummy_data

--- js/dummy_data.js
+++ js/dummy_data.js
@@ -1,16 +1,19 @@
 export default [
   {
     "id" : 'afsdhjklds',
+    "deck_id" : '27',
     "front" : 'hey',
     "back" : 'girl'
   },
   {
     "id" : 'asfdhjl',
+    "deck_id" : '27',
     "front" : 'Charlie',
     "back" : 'bit me'
   },
   {
     "id" : 'ouyiaerfs',
+    "deck_id" : '27',
     "front" : 'Jonny',
     "back" : 'be good'
   }
